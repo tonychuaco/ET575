92d19e78b98e6c11594d312dc1a4508b76fe094c
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2022-07-08 23:50:01 -0400

    Assignment1-2

---
+++ Assignment1-2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Shopping Cart");
+Console.WriteLine("-------------------------------");
+Console.Write("#\tItem\tCost");
+Console.WriteLine();
+Console.Write("1\tChalupa\t$1.50");
+Console.WriteLine();
+Console.Write("2\tSoft Taco Supreme\t$1.00");
+Console.WriteLine();
+Console.Write("1\tMeximelt\t$2.50");
+Console.WriteLine();
+Console.Write("\tSubtotal: \t$5.00");
---
+++ README.md
@@ -0,0 +1,12 @@
+# Assignment 1, #2
+
+C# console app to output in column format, e.g. a receipt layout.
+
+## User Stories
+
+- [x]  Use column format commands to create a program which 
+ outputs the content in columns.
+
+## Notes
+
+To Do: to fix the column formatting so the columns can be lined up.
