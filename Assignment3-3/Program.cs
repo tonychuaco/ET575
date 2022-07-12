efee83f73a3bad90f924ec88b7b38bf52408aa9d
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2022-07-11 23:26:40 -0400

    Added type casting

--- Assignment3-3/Program.cs
+++ Assignment3-3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float a, b;
+int c;
+Console.Write("Enter a floating-point value: ");
+a = float.Parse(Console.ReadLine());
+Console.Write("Enter a floating-point value: ");
+b = float.Parse(Console.ReadLine());
+c = (int) (a + b);
+Console.Write("Sum of the integers: " + c);
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Assignment 3: Variables, #3
 
+C# console app demonstrating type casting and outputting the sum of float inputs as type integer.
+
 ## User Stories
+
+- [x] Request two floating-point values from the console. 
+- [x] Output the sum of the integer portion of both values.
