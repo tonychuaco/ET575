2fee8bc0fd06879ae1b608dc3732871c465ecd5d
Anthony Djoewanda <16738612+tonychuaco@users.noreply.github.com>
2022-07-11 23:08:38 -0400

    Added implicit casting

--- Assignment3-2/Program.cs
+++ Assignment3-2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char a;
+int n;
+Console.Write("Input an uppercase character: ");
+a = char.Parse(Console.ReadLine());
+n = a + 32;
+a = (char) n;
+Console.WriteLine("The lowercase equivalent is '" + a + "'.");
